182774f9f4c08bfb45b7b872b1345819c61043a4
tanvir-22 <tanvir.mazumder01@gmail.com>
2026-05-27 16:51:29 +0600

    get ideas api done

--- index.js
+++ index.js
@@ -1,6 +1,43 @@
 const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
-const port = 5000;
+const cors = require('cors')
+require("dotenv").config();
+
+app.use(cors());
+const port = process.env.PORT;
+const uri = process.env.URI;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+
+    const database = client.db("Ideavault");
+    const posts = database.collection("posts");
+
+    app.get("/posts", async (req, res) => {
+      const allPostData = await posts.find({}).toArray();
+      res.send(allPostData);
+    });
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Server is running");
