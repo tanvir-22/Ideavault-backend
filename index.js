09218e49c47525e88f7038c87ed8ecc70927fbc5
tanvir-22 <tanvir.mazumder01@gmail.com>
2026-05-27 19:20:35 +0600

    create post api done

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
-const cors = require('cors')
+const cors = require("cors");
 require("dotenv").config();
-
+app.use(express.json());
 app.use(cors());
 const port = process.env.PORT;
 const uri = process.env.URI;
@@ -27,10 +27,43 @@ async function run() {
 
     const database = client.db("Ideavault");
     const posts = database.collection("posts");
+    const users = database.collection("user");
 
+    //retrieve all posts
     app.get("/posts", async (req, res) => {
       const allPostData = await posts.find({}).toArray();
       res.send(allPostData);
+    });
+
+    //retrieve a single post
+    app.get("/ideas/:id", async (req, res) => {
+      const { id } = req.params;
+      const query = { _id: new ObjectId(id) };
+      const requiredDocument = await posts.findOne(query);
+      res.send(requiredDocument);
+    });
+
+    app.get("/users/:id", async (req, res) => {
+      const { id } = req.params;
+      const query = { _id: new ObjectId(id) };
+      const user = await users.findOne(query);
+      res.send(user);
+    });
+
+    //create a post
+    app.post("/createpost", async (req, res) => {
+      const postData = req.body;
+      if(typeof postData.tags ==='string'){
+        postData.tags = postData.tags.split(',').map(t=>t.trim())
+      }
+      if(typeof postData.estimatedBudget!= 'Number'){
+        postData.estimatedBudget = Number(postData.estimatedBudget)
+      }
+      postData.createdAt = new Date();
+      postData.updatedAt = new Date();
+      const result = await posts.insertOne(postData);
+      res.send(result);
+      console.log("new idea inserted to collection");
     });
   } finally {
     // Ensures that the client will close when you finish/error
